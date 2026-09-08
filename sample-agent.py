07f16833de5f269258652ccbee85011baa3ae488
Sudharsana125 <sudharsanak13@gmail.com>
2026-09-08 21:47:26 +0530

    Add agent deep dive example

---
+++ sample-agent.py
@@ -0,0 +1,8 @@
+from crewai import Agent
+
+teacher = Agent(
+    role = "A teacher",
+    goal = "Teaching Physics to 10 th std student",
+    backstory = "You are a patient Physics teacher who enjoys helping students understand basic concepts."
+)
+print(teacher)
